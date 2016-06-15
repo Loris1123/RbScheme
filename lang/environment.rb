26363014ce0adcfe597846cd30cbe77a740c5547
Loris1123 <molottermann@gmail.com>
2016-06-15 19:58:37 +0200

    Environments have parents and look in parent if they do not have a symbol in local env

--- lang/environment.rb
+++ lang/environment.rb
@@ -1,15 +1,27 @@
+require_relative '../lang/errors'
+
 # Normal hashtable for now...will implement my own later
 class Environment
-  def initialize
+  def initialize(parent=nil)
     @environment = Hash.new
+    @parent = parent
   end
 
   def get(symbol)
-    @environment[symbol.value]
+    result = @environment[symbol.value]
+    if result == nil and @parent != nil
+      result = @parent.get(symbol)
+    end
+    return result
   end
 
   def put(symbol, value)
+    raise SchemeInternalError.new("Only Symbols are allowed as keys. Got #{symbol.class}") unless symbol.class == SchemeSymbol
     @environment[symbol.value] = value
+  end
+
+  def parent
+    @parent
   end
 
   def get_environment
