b6b92b3d4bcb1b2b786e0ad6282f0e2536c3a6d7
Loris1123 <molottermann@gmail.com>
2016-06-28 15:14:36 +0200

    Add builtins to symboltable

--- scheme.rb
+++ scheme.rb
@@ -8,6 +8,7 @@ require_relative 'eval'
 require_relative 'lang/environment'
 require_relative 'lang/builtinfunctions'
 require_relative 'lang/objects'
+require_relative 'lang/symboltable'
 
 if ARGV[1] != "skiptest"
   # Start tests
@@ -16,32 +17,29 @@ if ARGV[1] != "skiptest"
 end
 
 global_env = Environment.new()
-global_env.put(SchemeSymbol.new('+'), BuiltinFunction.new('SchemePlus', Functions.scheme_plus , 2, -1))
-global_env.put(SchemeSymbol.new('*'), BuiltinFunction.new('SchemeTimes', Functions.scheme_times , 2, -1))
-global_env.put(SchemeSymbol.new('-'), BuiltinFunction.new('SchemeSubstract', Functions.scheme_substract ,2, -1))
-global_env.put(SchemeSymbol.new('/'), BuiltinFunction.new('SchemeDivide', Functions.scheme_divide ,2, 2))
-global_env.put(SchemeSymbol.new('<'), BuiltinFunction.new('SchemeLowerThan', Functions.lower_than,2, 2))
-global_env.put(SchemeSymbol.new('>'), BuiltinFunction.new('SchemeGreaterThan', Functions.greater_than ,2, 2))
-global_env.put(SchemeSymbol.new('modulo'), BuiltinFunction.new('SchemeModulo', Functions.scheme_modulo , 2, 2))
-global_env.put(SchemeSymbol.new('abs'), BuiltinFunction.new('SchemeAbs', Functions.scheme_abs , 1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('+')), BuiltinFunction.new('SchemePlus', Functions.scheme_plus , 2, -1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('-')), BuiltinFunction.new('SchemeSubstract', Functions.scheme_substract ,2, -1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('*')), BuiltinFunction.new('SchemeTimes', Functions.scheme_times , 2, -1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('/')), BuiltinFunction.new('SchemeDivide', Functions.scheme_divide ,2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('<')), BuiltinFunction.new('SchemeLowerThan', Functions.lower_than,2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('>')), BuiltinFunction.new('SchemeGreaterThan', Functions.greater_than ,2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('modulo')), BuiltinFunction.new('SchemeModulo', Functions.scheme_modulo , 2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('abs')), BuiltinFunction.new('SchemeAbs', Functions.scheme_abs , 1, 1))
 
-global_env.put(SchemeSymbol.new('eq?'), BuiltinFunction.new('SchemeEquals', Functions.scheme_equals ,2, 2))
-global_env.put(SchemeSymbol.new('integer?'), BuiltinFunction.new('SchemeInteger?', Functions.integer? ,1, 1))
-global_env.put(SchemeSymbol.new('symbol?'), BuiltinFunction.new('SchemeSymbol?', Functions.symbol? ,1, 1))
-global_env.put(SchemeSymbol.new('string?'), BuiltinFunction.new('SchemeString?', Functions.string? ,1, 1))
-global_env.put(SchemeSymbol.new('function?'), BuiltinFunction.new('SchemeFunction?', Functions.function? ,1, 1))
-global_env.put(SchemeSymbol.new('builtin-function?'), BuiltinFunction.new('SchemeBuiltinFunction?', Functions.builtin_function? ,1, 1))
-global_env.put(SchemeSymbol.new('syntax?'), BuiltinFunction.new('SchemeSyntax?', Functions.syntax? ,1, 1))
-global_env.put(SchemeSymbol.new('builtin-syntax?'), BuiltinFunction.new('SchemeBuiltinSyntax?', Functions.builtin_syntax? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('eq?')), BuiltinFunction.new('SchemeEquals', Functions.scheme_equals ,2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('integer?')), BuiltinFunction.new('SchemeInteger?', Functions.integer? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('symbol?')), BuiltinFunction.new('SchemeSymbol?', Functions.symbol? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('string?')), BuiltinFunction.new('SchemeString?', Functions.string? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('function?')), BuiltinFunction.new('SchemeFunction?', Functions.function? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('builtin-function?')), BuiltinFunction.new('SchemeBuiltinFunction?', Functions.builtin_function? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('syntax?')), BuiltinFunction.new('SchemeSyntax?', Functions.syntax? ,1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('builtin-syntax?')), BuiltinFunction.new('SchemeBuiltinSyntax?', Functions.builtin_syntax? ,1, 1))
 
-global_env.put(SchemeSymbol.new('cons'), BuiltinFunction.new('SchemeCons', Functions.scheme_cons, 2, 2))
-global_env.put(SchemeSymbol.new('cons?'), BuiltinFunction.new('SchemeCons?', Functions.scheme_cons?, 1, 1))
-global_env.put(SchemeSymbol.new('car'), BuiltinFunction.new('SchemeCar', Functions.scheme_car, 1, 1))
-global_env.put(SchemeSymbol.new('cdr'), BuiltinFunction.new('SchemeCdr', Functions.scheme_cdr, 1, 1))
-global_env.put(SchemeSymbol.new('define'), BuiltinSyntax.new('SchemeDefine', Functions.scheme_define, 2, 2))
-
-global_env.put(SchemeSymbol.new('a'), SchemeInteger.new(10))
-global_env.put(SchemeSymbol.new('b'), SchemeInteger.new(20))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('cons')), BuiltinFunction.new('SchemeCons', Functions.scheme_cons, 2, 2))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('cons?')), BuiltinFunction.new('SchemeCons?', Functions.scheme_cons?, 1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('car')), BuiltinFunction.new('SchemeCar', Functions.scheme_car, 1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('cdr')), BuiltinFunction.new('SchemeCdr', Functions.scheme_cdr, 1, 1))
+global_env.put(Symboltable.get_or_add(SchemeSymbol.new('define')), BuiltinSyntax.new('SchemeDefine', Functions.scheme_define, 2, 2))
 
 input = ARGV[0]
 if input != nil
